c621d547545031b6269cc779ae4956b54fd8ef24
shivanitomar1995@gmail.com <shivanitomar1995@gmail.com>
2019-03-03 13:43:39 +0530

    build is ready to go

---
+++ build/lib/example_pkg/__init__.py
@@ -0,0 +1,11 @@
+class Test:
+    def __init__(self):
+        print("class has been initialized and object is created")
+
+
+    def test1(self):
+        print("this function is for testing purpose")
+
+
+
+        
